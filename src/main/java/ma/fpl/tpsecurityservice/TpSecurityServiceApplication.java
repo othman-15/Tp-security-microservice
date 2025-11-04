18c3b3ac118ec4038cf5cd7f3ffbbe5d5d0c59a3
othman-ouseffar <qudss2020@gmail.com>
2025-11-04 19:41:54 +0100

    first commit

---
+++ src/main/java/ma/fpl/tpsecurityservice/TpSecurityServiceApplication.java
@@ -0,0 +1,23 @@
+package ma.fpl.tpsecurityservice;
+
+import ma.fpl.tpsecurityservice.configuration.RsaKeys;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+@EnableConfigurationProperties(RsaKeys.class)
+public class TpSecurityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSecurityServiceApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
